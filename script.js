631748d7b2c606a366248b24b5b2fe2c04e7dbd7
acckox <138218341+acckox@users.noreply.github.com>
2024-10-25 18:12:20 +0800

    visual change

--- script.js
+++ script.js
@@ -1,8 +1,70 @@
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+function handleScrollAnimation() {
+    const sections = document.querySelectorAll('.fade-in-section');
+    
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+}
+
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 todoButton.addEventListener('click', addTodo);
+todoInput.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        addTodo();
+    }
+});
 
 function addTodo() {
     if (todoInput.value.trim() === '') return;
@@ -25,12 +87,6 @@ function addTodo() {
     todoInput.value = '';
 }
 
-todoInput.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-        addTodo();
-    }
-});
-
 const projectCards = document.querySelectorAll('.project-card');
 projectCards.forEach(card => {
     card.addEventListener('mousedown', () => {
@@ -41,3 +97,27 @@ projectCards.forEach(card => {
     });
 });
 
+document.addEventListener('mousemove', function(e) {
+    const header = document.querySelector('header');
+    const interactiveBg = document.createElement('div');
+    interactiveBg.classList.add('interactive-bg');
+    interactiveBg.style.setProperty('--x', `${e.clientX}px`);
+    interactiveBg.style.setProperty('--y', `${e.clientY}px`);
+    header.appendChild(interactiveBg);
+
+    setTimeout(() => {
+        interactiveBg.remove();
+    }, 1000);
+});
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    handleScrollAnimation();
+};
